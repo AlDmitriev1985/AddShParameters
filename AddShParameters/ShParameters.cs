cfe536f25d5182f9894a822f17aca4d92b36ffb1
andreybox83@gmail.com <andreybox83@gmail.com>
2021-07-28 09:51:18 +0300

    Add project files.

---
+++ AddShParameters/ShParameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace AddShParameters
+{
+    public class ShParameters
+    {
+        public Guid PGUID { get; set; }
+        public string PName { get; set; }
+        public ParameterType PDataType { get; set; }
+        public DefinitionGroup PGroup { get; set; }
+        public string PDescription { get; set; }
+        public BuiltInParameterGroup PDataCategory { get; set; }
+        public bool PIsInstance { get; set; }
+        public ExternalDefinition PexternalDefinition { get; set; }
+
+        public ShParameters()
+        {
+        }
+    }
+
+}
